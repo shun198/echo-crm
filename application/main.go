9d98b60e9f0f703ca8d0d14b196b04441e660c59
shun198 <shunhiroseluvmri@gmail.com>
2024-04-30 14:03:51 +0900

    👍 add air.toml

--- application/main.go
+++ application/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hot Reload Working!!")
+		return c.String(http.StatusOK, "Hot Reload")
 	})
 	e.Logger.Fatal(e.Start(":8000"))
 }

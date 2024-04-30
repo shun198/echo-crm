e75959fd0cbebfe9fa1dcb0e0b840d5779a8ef20
shun198 <shunhiroseluvmri@gmail.com>
2024-04-30 14:13:45 +0900

    👍 generate docs

--- application/main.go
+++ application/main.go
@@ -4,10 +4,14 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/swaggo/echo-swagger"
+	"github.com/shun198/go-crm/application/docs"
 )
 
 func main() {
 	e := echo.New()
+	// https://github.com/swaggo/echo-swagger?tab=readme-ov-file
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hot Reload")
 	})

0301da96d3903d413ab388c3982102b00ac2a21c
shun198 <shunhiroseluvmri@gmail.com>
2024-04-30 16:29:14 +0900

    👍 change doc

--- application/docs/docs.go
+++ application/docs/docs.go
@@ -19,11 +19,11 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "",
+	Version:          "1.0",
 	Host:             "",
 	BasePath:         "",
 	Schemes:          []string{},
-	Title:            "",
+	Title:            "Swagger Example API",
 	Description:      "",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
--- application/main.go
+++ application/main.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"net/http"
-	// https://github.com/labstack/echo-contrib/issues/8
+
 	_ "github.com/shun198/go-crm/docs"
 
+	// https://github.com/labstack/echo-contrib/issues/8
 	"github.com/labstack/echo/v4"
 	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
-// @title Swagger Example API
-// @version 1.0
+// @title	Swagger Example API
+// @version	1.0
 func main() {
 	e := echo.New()
 	// https://github.com/swaggo/echo-swagger?tab=readme-ov-file
 	// https://medium.com/@chaewonkong/a-five-step-guide-to-integrating-swagger-with-echo-in-go-79be49cfedbe
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hot Reload")
+		return c.String(http.StatusOK, "Hello World")
+	})
+	e.GET("/health", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{"msg": "pass"})
 	})
 	e.Logger.Fatal(e.Start(":8000"))
 }

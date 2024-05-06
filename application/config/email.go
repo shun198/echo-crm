eee86bcea091477039c8756dd956e76eb67e752c
shun198 <shunhiroseluvmri@gmail.com>
2024-05-06 21:38:03 +0900

    👍 add email.go

---
+++ application/config/email.go
@@ -0,0 +1,138 @@
+package config
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"html/template"
+	"os"
+	"strconv"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ses"
+	"github.com/labstack/echo/v4"
+	"gopkg.in/gomail.v2"
+)
+
+type UrlData struct {
+	Url string
+}
+
+type emailTemplates struct {
+	html      string
+	plaintext string
+}
+
+func sendEmail(e *echo.Echo, templates emailTemplates, email string, subject string) error {
+	m := gomail.NewMessage()
+	m.SetHeader("From", os.Getenv("MAIL_ADMIN"))
+	m.SetHeader("To", email)
+	m.SetHeader("Subject", subject)
+	m.SetBody("text/html", templates.html)
+	m.AddAlternative("text/plain", templates.plaintext)
+
+	host := os.Getenv("MAIL_HOST")
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	username := os.Getenv("MAIL_USERNAME")
+	password := os.Getenv("MAIL_PASSWORD")
+
+	d := gomail.NewDialer(host, port, username, password)
+
+	if err := d.DialAndSend(m); err != nil {
+		e.Logger.Error(err)
+		return errors.New("メールの送信に失敗しました")
+	}
+	return nil
+}
+
+func sendEmailSES(e *echo.Echo, templates emailTemplates, email string, subject string) error {
+	charset := "UTF-8"
+
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(os.Getenv("AWS_REGION"))},
+	)
+	if err != nil {
+		return err
+	}
+
+	// Create an SES session.
+	svc := ses.New(sess)
+
+	// Assemble the email.
+	input := &ses.SendEmailInput{
+		Destination: &ses.Destination{
+			CcAddresses: []*string{},
+			ToAddresses: []*string{
+				aws.String(email),
+			},
+		},
+		Message: &ses.Message{
+			Body: &ses.Body{
+				Html: &ses.Content{
+					Charset: aws.String(charset),
+					Data:    aws.String(templates.html),
+				},
+				Text: &ses.Content{
+					Charset: aws.String(charset),
+					Data:    aws.String(templates.plaintext),
+				},
+			},
+			Subject: &ses.Content{
+				Charset: aws.String(charset),
+				Data:    aws.String(subject),
+			},
+		},
+		Source: aws.String(os.Getenv("MAIL_ADMIN")),
+	}
+	result, err := svc.SendEmail(input)
+
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case ses.ErrCodeMessageRejected:
+				e.Logger.Error(ses.ErrCodeMessageRejected, aerr.Error())
+			case ses.ErrCodeMailFromDomainNotVerifiedException:
+				e.Logger.Error(ses.ErrCodeMailFromDomainNotVerifiedException, aerr.Error())
+			case ses.ErrCodeConfigurationSetDoesNotExistException:
+				e.Logger.Error(ses.ErrCodeConfigurationSetDoesNotExistException, aerr.Error())
+			default:
+				e.Logger.Error(aerr.Error())
+			}
+		} else {
+			e.Logger.Error(err.Error())
+		}
+
+		return err
+	}
+
+	e.Logger.Info("Email Sent to address: " + email)
+	e.Logger.Info(result)
+	return nil
+}
+
+func CreateEmail(e *echo.Echo, email string, url string, templateName string, subject string) error {
+	wd, _ := os.Getwd()
+
+	html, _ := template.New(fmt.Sprint(templateName, ".html")).ParseFiles(fmt.Sprint(wd, "/templates/email/", templateName, ".html"))
+	plaintext, _ := template.New(fmt.Sprint(templateName, ".txt")).ParseFiles(fmt.Sprint(wd, "/templates/email/", templateName, ".txt"))
+
+	var tpl bytes.Buffer
+	if err := html.Execute(&tpl, UrlData{Url: url}); err != nil {
+		e.Logger.Error(err)
+		return err
+	}
+	htmlResult := tpl.String()
+	if err := plaintext.Execute(&tpl, UrlData{Url: url}); err != nil {
+		e.Logger.Error(err)
+		return err
+	}
+	plaintextResult := tpl.String()
+
+	if os.Getenv("DEBUG") == "" {
+		return sendEmailSES(e, emailTemplates{html: htmlResult, plaintext: plaintextResult}, email, subject)
+	} else {
+		return sendEmail(e, emailTemplates{html: htmlResult, plaintext: plaintextResult}, email, subject)
+	}
+}
--- application/go.mod
+++ application/go.mod
@@ -11,6 +11,7 @@ require (
 require (
 	dario.cat/mergo v1.0.0 // indirect
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/aws/aws-sdk-go v1.52.2 // indirect
 	github.com/bep/godartsass v1.2.0 // indirect
 	github.com/bep/godartsass/v2 v2.0.0 // indirect
 	github.com/bep/golibsass v1.1.1 // indirect
@@ -43,6 +44,7 @@ require (
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
@@ -76,6 +78,8 @@ require (
 	golang.org/x/time v0.5.0 // indirect
 	golang.org/x/tools v0.20.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/postgres v1.5.7 // indirect
